36616426d5c1a275672eaeed9be015068fab0685
wwwooo <malinovsvska@gmail.com>
2016-09-28 12:38:07 +0300

    added modal windiw

--- js/main.js
+++ js/main.js
@@ -1,57 +1,51 @@
-//'use strict';
+'use strict';
 $(function() {
-    var quiz = [
-        {
-            question: 'Какие опасные последствия могут возникнуть при торможении автомобиля с различным износом шин правых и левых колес?',
-            answers: ['Отслоение протектора.', 'Занос с возможным опрокидыванием автомобиля.', 'Перегрев тормозных механизмов.'],
-        }, {
-            question: 'В какое время суток разрешается перегонять стадо животных по дороге?',
-            answers: ['Разрешается в любое время суток при наличии необходимого количества погонщиков.','Разрешается в светлое время суток при наличии двух погонщиков.', 'Разрешается только в светлое время суток, при этом привлекается такое количество погонщиков, чтобы можно было направлять животных как можно ближе к правому краю дороги.']
-        }, {
-            question: 'Имеют ли преимущество сигналы регулировщика перед требованиями дорожных знаков?',
-            answers: ['Имеют преимущество только перед требованиями дорожных знаков приоритета и являются обязательными для выполнения.', 'Имеют преимущество перед требованиями всех дорожных знаков и являются обязательными для выполнения.', 'Не имеют преимущества перед дорожными знаками.']
-        }
-    ];
-
-    // localStorage.setItem('quiz', JSON.stringify(quiz));
-    var str = localStorage.getItem('quiz');
-    var data = JSON.parse(str);
-    console.dir(data);
-
-    var addAttrs = function(obj, attributes) {
-        for (var key in attributes) {
-            obj.setAttribute(key, attributes[key]);
-        }
+    var quiz = {
+        what_test: 'Тест по ПДД',
+        questions: [
+            {
+                question: 'Какие опасные последствия могут возникнуть при торможении автомобиля с различным износом шин правых и левых колес?',
+                answers: ['Отслоение протектора.', 'Занос с возможным опрокидыванием автомобиля.', 'Перегрев тормозных механизмов.'],
+                right_answer: 1
+            }, {
+                question: 'В какое время суток разрешается перегонять стадо животных по дороге?',
+                answers: ['Разрешается в любое время суток при наличии необходимого количества погонщиков.','Разрешается в светлое время суток при наличии двух погонщиков.', 'Разрешается только в светлое время суток, при этом привлекается такое количество погонщиков, чтобы можно было направлять животных как можно ближе к правому краю дороги.'],
+                right_answer: 2
+            }, {
+                question: 'Имеют ли преимущество сигналы регулировщика перед требованиями дорожных знаков?',
+                answers: ['Имеют преимущество только перед требованиями дорожных знаков приоритета и являются обязательными для выполнения.', 'Имеют преимущество перед требованиями всех дорожных знаков и являются обязательными для выполнения.', 'Не имеют преимущества перед дорожными знаками.'],
+                right_answer: 0
+            }
+        ]
     };
 
-    var createElem = function(tag, parent, text, attrs) {
-        var elem = document.createElement(tag);
-        elem.innerHTML = text || '';
-        if (attrs) {
-            addAttrs(elem, attrs);
-        }
-        return parent.appendChild(elem);
-    };
-
-    var wrap = document.createElement('div');
-    var h1 = createElem('h1', wrap, 'Тест по ПДД');
-    var form = createElem('form', wrap);
-    var ol = createElem('ol', form);
-
-    for (var i = 0; i < 3; i++) {
-        var li = createElem('li', ol);
-        var span = createElem('span', li, data[i].question);
-
-        for (var n = 0; n < 3; n++) {
-            var label = createElem('label', li, data[i].answers[n]);
-            var radio = createElem('input', label, null, {'type': 'radio', 'name': 'radio-' + i});
-            var span = createElem('span', label);
-        }
-    }
-
-    var submit = createElem('input', form, null, {type: 'submit', value: 'Проверить мои результаты'});
-
-    var html = wrap.innerHTML;
+    localStorage.setItem('quiz', JSON.stringify(quiz));
+    var html = $('#item_tmpl').html();
+    var data = JSON.parse(localStorage.getItem('quiz'));
     var content = tmpl(html, data);
+
     $('body').append(content);
+
+    var hideModal = function() {
+        location.reload();
+    };
+    var showModal = function(count) {
+        $('body').append('<div class="overlay"></div><div class="modal"><h1>' + count + '/3</h1></div>');
+        $('.overlay').one('click', hideModal);
+    };
+
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var countAnswers = 0;
+        $(this).find('li').each(function(i) {
+            var chs = $(this).find(':checked')[0];
+            if (chs) {
+                if (chs.value == data.questions[i].right_answer) {
+                    countAnswers++;
+                }
+            }
+        });
+        showModal(countAnswers);
+    });
 });
